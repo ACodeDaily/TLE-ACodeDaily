a536d0678109f1191c8323de5ae43e51c069c1ab
vedantmishra69 <vedantmishra050302@gmail.com>
2024-01-28 01:50:55 +0530

    Small changes

--- tle/cogs/acd_ai.py
+++ tle/cogs/acd_ai.py
@@ -1,58 +1,53 @@
 from discord.ext import commands
-from collections.abc import AsyncIterable
 import discord
+import logging
 import google.generativeai as genai
-from tle.util import settings
+from tle.util import gemini_model_settings as settings
 import requests
 from PIL import Image
 from tle import constants
-from ratelimit import limits, RateLimitException, sleep_and_retry
+from ratelimit import limits, sleep_and_retry
 
 
 
 ONE_MINUTE = 60
 MAX_CALLS_PER_MINUTE = 60
-API_KEY = constants.API_KEY
+GEMINI_API_KEY = constants.GEMINI_API_KEY
 
-image_types = ["jpeg", "png", "webp", "heic", "heif"]
-no_response_message = "Sorry, can't answer that. possible reasons might be no response, recitation, safety issue or blocked content."
-help_text = "```\nUsage:\n$chat : to create a public thread to interact with the bot.\n$private : to create a private thread to interact with the bot.\n\nACD AI answers to all text, image and attachment.\nHave fun!\n\nfor Admin/Mods:\n$set : to set a channel.\n$unset : to unset a channel.\n$show : to show current channels.\n$clear : to delete all the threads in set channels.```"
-allowed_guilds = [1063393625049923687, 501032525284769842]
-dev_id = 501026469569363988
+IMAGE_TYPES = ["jpeg", "png", "webp", "heic", "heif"]
+NO_RESPONSE_MESSAGE = "Sorry, can't answer that. possible reasons might be no response, recitation, safety issue or blocked content."
+HELP_TEXT = "```\nUsage:\n$chat : to create a public thread to interact with the bot.\n$private : to create a private thread to interact with the bot.\n\nACD AI answers to all text, image and attachment.\nHave fun!\n\nfor Admin/Mods:\n$set : to set a channel.\n$unset : to unset a channel.\n$show : to show current channels.\n$clear : to delete all the threads in set channels.```"
+ALLOWED_GUILDS = [1063393625049923687, 501032525284769842]
+DEV_ID = 501026469569363988
+logger = logging.getLogger(__name__)
 
-class ACDAI(commands.Cog):
+class ACD_AI(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.allowed_channels = []
-        self.allowed_guilds = allowed_guilds
-        self.dev_id = dev_id
-        genai.configure(api_key=API_KEY)
+        self.allowed_channels: list = []
+        self.allowed_guilds: list = ALLOWED_GUILDS
+        self.dev_id: int = DEV_ID
+        genai.configure(api_key=GEMINI_API_KEY)
         self.text_model = genai.GenerativeModel(model_name="gemini-pro",
-                              generation_config=settings.text_generation_config, # type: ignore
+                              generation_config=settings.text_generation_config, 
                               safety_settings=settings.text_safety_settings)
         self.image_model = genai.GenerativeModel(model_name="gemini-pro-vision",
-                              generation_config=settings.image_generation_config, # type: ignore
+                              generation_config=settings.image_generation_config, 
                               safety_settings=settings.image_safety_settings)
-
-        self.chats = {}
+        self.chats: dict = {}
     
     async def print_response(self, response, message):
         async with message.channel.typing():
             strings = []
-            string = []
-            count = 0
-            for char in response:
-                if count < 2000: string.append(char); count += 1
-                else: strings.append("".join(string)); string = [char]; count = 1
-            strings.append("".join(string))
+            for index in range(0, len(response), 2000):
+                strings.append(response[index: min(len(response), index + 2000)])
             try:
                 await message.reply(strings[0])
                 for string in strings[1:]:
                     await message.channel.send(string)
             except Exception as e:
                 message.reply("No response, discord issue :(")
-                print("last message:", message.content)
-                print(e)
+                logger.warn(f"last message: {message.content}\n{e}".format())
     
     @sleep_and_retry
     @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
@@ -65,35 +60,32 @@ class ACDAI(commands.Cog):
                 try:
                     response = chat.send_message(message.content).text
                 except Exception as e:
-                    response = no_response_message
-                    print("last message:", message.content)
-                    print(e)
+                    response = NO_RESPONSE_MESSAGE
+                    logger.warn(f"last message: {message.content}\n{e}".format())
             elif attachment.content_type.split('/')[0] == "image":
-                if attachment.content_type.split('/')[1] in image_types:
+                if attachment.content_type.split('/')[1] in IMAGE_TYPES:
                     try:
                         image = Image.open(requests.get(attachment.url, stream = True).raw)
                         response = self.image_model.generate_content([message.content, image] if message.content else image).text
                     except Exception as e:
                         response = "Unable to process that image."
-                        print("last message:", attachment.url)
-                        print(e)
+                        logger.warn(f"last message: {attachment.url}\n{e}".format())
                 else: response = "Invalid image format. please use JPEG, PNG, WEBP, HEIC or HEIF"
             else:
                 try:
                     att_content = requests.get(attachment.url).content.decode()
                 except Exception as e:
                     response = "Format not supported: " + attachment.filename
-                    print("last message:", attachment.url)
-                    print(e)
+                    logger.warn(f"last message: {attachment.url}\n{e}".format())
                 else:
                     try:
                         response = chat.send_message(message.content + '\n\n' + att_content)
                     except Exception as e:
-                        response = no_response_message
+                        response = NO_RESPONSE_MESSAGE
         self.bot.loop.create_task(self.print_response(response, message))
     
     def has_permissions(self, message):
-        return (message.author.guild_permissions.manage_channels and message.guild.id in allowed_guilds) or message.author.id == dev_id
+        return (message.author.guild_permissions.manage_channels and message.guild.id in self.allowed_guilds) or message.author.id == self.dev_id
     
     async def start_thread(self, message, is_public):
         try:
@@ -105,7 +97,7 @@ class ACDAI(commands.Cog):
             self.chats[str(thread.id)] = self.text_model.start_chat(history=[{'role':'user', 'parts': ["hi"]}, {'role': 'model', 'parts': [hello_text]}])
         except Exception as e:
             await message.channel.send("Could not start a thread :(")
-            print(e)
+            logger.warn(e)
     
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -114,11 +106,11 @@ class ACDAI(commands.Cog):
         if message.content.startswith("$show"):
             if self.has_permissions(message):
                 set_channels = []
-                count = 1
+                channel_count = 1
                 for Id in self.allowed_channels:
                     if message.guild.get_channel(Id):
-                        set_channels.append(f"\n{count}. {message.guild.get_channel(Id).name}")
-                        count += 1
+                        set_channels.append(f"\n{channel_count}. {message.guild.get_channel(Id).name}")
+                        channel_count += 1
                 if set_channels:
                     await message.channel.send("Current channels:" + "".join(set_channels))
                 else:
@@ -140,7 +132,7 @@ class ACDAI(commands.Cog):
                 await message.channel.send("you don't have ``Manage channels`` permission!")
 
         if message.content.startswith("$help"):
-            await message.channel.send(help_text)
+            await message.channel.send(HELP_TEXT)
 
         if message.content.startswith("$unset"):
             if self.has_permissions(message):
@@ -156,18 +148,18 @@ class ACDAI(commands.Cog):
             if message.author.guild_permissions.manage_threads:
                 for Id in self.allowed_channels:
                     if message.guild.get_channel(Id):
-                        count = 0
+                        thread_count = 0
                         async with message.channel.typing():
                             for thread in message.guild.get_channel(Id).threads:
                                 await thread.delete()
-                                count += 1
-                        await message.channel.send(f"{count} threads deleted for ``{message.guild.get_channel(Id).name}``.")
+                                thread_count += 1
+                        await message.channel.send(f"{thread_count} threads deleted for ``{message.guild.get_channel(Id).name}``.")
             else:
                 await message.channel.send("you don't have ``Manage threads`` permission!")
 
 
         if str(message.channel.id) in self.chats:
-            self.bot.loop.create_task(self.get_response(message, self.chats[str(message.channel.id)])) # type: ignore
+            self.bot.loop.create_task(self.get_response(message, self.chats[str(message.channel.id)]))
         elif message.content:
             first = message.content.split()[0]
             if first == "$chat":
@@ -182,4 +174,4 @@ class ACDAI(commands.Cog):
                     await message.channel.send("This channel has not been set. type $set to set it.")
 
 async def setup(bot):
-    await bot.add_cog(ACDAI(bot))
+    await bot.add_cog(ACD_AI(bot))
